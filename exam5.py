b8099e78c3a22af0c858b8130378e92599ee2100
scsv12345 <scsv123@naver.com>
2015-12-04 00:10:02 +0900

    Create exam5.py

---
+++ exam5.py
@@ -0,0 +1,9 @@
+import urllib
+
+test=urllib.FancyURLopener()
+# it is not checking dault . so better than 
+# test=urllib.URLopener()
+
+test.retrieve("http://advod.smartmediarep.com/2015_11/lotterentacar_15_720.mp4-muxed.mp4","testout.mp4")
+
+# video downloder , 2015-12-04 Lee han chul.
